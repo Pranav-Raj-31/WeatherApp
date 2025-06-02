db791402e94c85df5bbb2a1ca051a091458e593d
Pranav-Raj-31 <pranavraj3105@gmail.com>
2025-06-02 11:45:37 +0530

    Initial commit

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://github.com/Pranav-Raj-31"
+        > Pranav Raj</a>,
+         <a href="https://github.com/Pranav-Raj-31">Open sourced on Github.</a>
+    </p>
+  </div>
+  );
+}
